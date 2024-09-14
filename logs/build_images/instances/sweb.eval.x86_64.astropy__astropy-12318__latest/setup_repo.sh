6f743f2f5a5037a9e0a82b1c409d718f3d335607
Muhammad Saadan <msaadan.bese21seecs@seecs.edu.pk>
2024-09-14 11:23:23 +0000

    Complete Setup

---
+++ logs/build_images/instances/sweb.eval.x86_64.astropy__astropy-12318__latest/setup_repo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euxo pipefail
+git clone -o origin https://github.com/astropy/astropy /testbed
+chmod -R 777 /testbed
+cd /testbed
+git reset --hard 43ce7895bb5b61d4fab2f9cc7d07016cf105f18e
+git remote remove origin
+source /opt/miniconda3/bin/activate
+conda activate testbed
+echo "Current environment: $CONDA_DEFAULT_ENV"
+sed -i 's/requires = \["setuptools",/requires = \["setuptools==68.0.0",/' pyproject.toml
+python -m pip install -e .[test] --verbose
